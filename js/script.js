a869de81cc9087a73b90a3cc91d1c7f4b1192ab1
Vincent <23vincent86@gmail.com>
2020-09-02 12:27:34 +0200

    index

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
-alert();
+// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button)
+// in cui possiamo scrivere completamente o parzialmente il nome di un film.
+// Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono
+// ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori
+// per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+// {
+//     "page": 1,
+//     "total_results": 4,
+//     "total_pages": 1,
+//     "results": [
+//         {
+//             "popularity": 60.332,
+//             "vote_count": 17638,
+//             "video": false,
+//             "poster_path": "/xvjOBWfm32skiwIAU2iocb6fyC8.jpg",
+//             "id": 120,
+//             "adult": false,
+//             "backdrop_path": "/vRQnzOn4HjIMX4LBq9nHhFXbsSu.jpg",
+//             "original_language": "en",
+//             "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+//             "genre_ids": [
+//                 28,
+//                 12,
+//                 14
+//             ],
+//             "title": "Il Signore degli Anelli - La compagnia dell'anello",
+//             "vote_average": 8.3,
+//             "overview": "Bilbo Baggins ha deciso di celebrare il suo centoundicesimo compleanno in maniera molto particolare;alla fine della festa, abbandona parenti, amici, conoscenti e il suo affezionato nipote Frodo, cui lascia ogni cosa. Lo stregone Gandalf insiste perché lo hobbit lasci a Frodo anche il suo anello magico, su cui intende tenere un occhio vigile e fare alcune ricerche, perché potrebbe celare un mistero più inquietante di quanto non sembri. E così è; quello di Bilbo è l'Unico Anello, l'Anello per domarli, il cuore del potere del malefico Signore Oscuro Sauron, che sta tornando a tessere le sue trame di conquista e distruzione. Per impedirgli di vedere realizzato il suo sanguinoso disegno, l'anello dovrà essere distrutto, cosa possibile soltanto nella fornace di Monte Fato, dove Sauron, secoli prima, lo forgiò.",
+//             "release_date": "2001-12-18"
+//         },
+
+$(document).ready(function() {
+
+});
