a9f15e886353a243fe3ec1d53b14bb879325e02c
Vincent <23vincent86@gmail.com>
2020-09-03 15:43:55 +0200

    mil one

--- js/script.js
+++ js/script.js
@@ -10,25 +10,38 @@
 // Lingua
 // Voto
 
+// Milestone 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5,
+// così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5,
+// lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva,
+// non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera
+// della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione
+// ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv.
+// Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query,
+// sia le serie tv, stando attenti ad avere alla fine dei valori simili
+// (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+
 
 $(document).ready(function() {
-
     $('#btg-search').click(function() {
-
-        searchFilm();
-        
+        var ricercaFilm = $('#inp').val();
+        reset ();
+        searchFilm(ricercaFilm);
     });
 });
 
 
 //*** FUNZIONI ***
 
-function searchFilm() {
-
-    var ricercaFilm = $('#inp').val();
-
+function reset() {
     $('.list-film').empty();
+    $('#inp').val('');
+};
 
+function searchFilm(data) {
     $.ajax(
     {
         url:'https://api.themoviedb.org/3/search/movie',
@@ -36,29 +49,50 @@ function searchFilm() {
         data:{
             api_key: '50aabbde5596b4f1a538a2fbc3fe6ee9',
             language:'it-IT',
-            query: ricercaFilm
+            query: data
         },
         success: function(risposta){
-            for(var i = 0; i < risposta.results.length; i++) {
-
-                var source = $("#film-template").html();
-
-                var template = Handlebars.compile(source);
-
-                var context = {
-                    title: risposta.results[i].title,
-                    original_title: risposta.results[i].original_title,
-                    original_language: risposta.results[i].original_language,
-                    vote_average: risposta.results[i].vote_average
-                };
-
-                var html = template(context);
-                $('.list-film').append(html);
-            };
+            if (risposta.total_results > 0) {
+                    printFilm(risposta.results);
+                } else {
+                    noResults();
+                }
         },
         error: function(){
             alert('errore');
         }
     }
     )
+};
+
+function printFilm(data) {
+    var source = $("#film-template").html();
+    var template = Handlebars.compile(source);
+
+    for(var i = 0; i < data.length; i++) {
+
+        var context = {
+            title: data[i].title,
+            original_title: data[i].original_title,
+            original_language: data[i].original_language,
+            vote_average: data[i].vote_average
+        };
+
+        var html = template(context);
+        $('.list-film').append(html);
+    };
+};
+
+function noResult() {
+    var source = $("#film-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+        noResult: "non ci sono risultati"
+    };
+    var html = template(context);
+    $(".lista-film").append(html);
+};
+
+function flag(stringa) {
+
 };
